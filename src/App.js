027f773d63409e510e8935ca5bb961821fb02629
FrodeBerg <bergstedt.frode@gmail.com>
2023-03-22 21:54:51 +0100

    Added header and Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
 
 function App() {
   return (
-    <div>
+    <div className='page'>
+      <Header />
+      <Footer />
     </div>
   );
+}
+
+class Header extends React.Component {
+  render() {
+    return (  
+    <div className='header'>
+      <h1>Cv Application</h1>
+    </div>)
+  }
+}
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <div className='footer'>
+        <h4>Frolle Productions</h4>
+      </div>
+    )
+  }
 }
 
 export default App;
